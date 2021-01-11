138068143c63d8d1abefe19111702fd3ae5b7cda
Luciano A. Campos <74323484+CamposDesigner@users.noreply.github.com>
2021-01-11 12:47:35 -0600

    Add files via upload

---
+++ rspec_exercise_3/rspec_exercise_3_Luciano_Campos/lib/part_1.rb
@@ -0,0 +1,38 @@
+
+
+    def is_prime?(num)
+        (2...num).each do |factor|
+           return false if num % factor == 0
+        end
+
+        num > 1
+    end
+
+
+
+    def nth_prime(n)
+        count = 0
+      
+        num = 1
+        while count < n
+          num += 1
+          count += 1 if is_prime?(num)
+        end
+      
+        num
+    end
+
+
+    def prime_range(num_min, num_max)
+        primes = []
+
+        (num_min..num_max).each do |num|
+            primes << num if is_prime?(num)
+        end
+    
+        primes
+    end
+
+
+
+
